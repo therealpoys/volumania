5d9094f7c23774c28086f805678caa15b041b503
jannik.karb <jannik.karb@concat.de>
2025-07-14 19:58:02 +0200

    Fix import issues change to one folder

--- backend/app/routers/autoscaler.py
+++ backend/app/routers/autoscaler.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-import auth
-from services import k8s_service
+from app import auth
+from app.services import k8s_service
 
 
 router = APIRouter()
--- backend/app/routers/system.py
+++ backend/app/routers/system.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-import auth
+from app import auth
 
 
 
